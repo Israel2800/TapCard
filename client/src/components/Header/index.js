23891d2b686577d6c30b50a3ce300ea8954177c5
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-09-21 18:19:36 -0500

    logo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/home">
-          <h1><img src={image} height={100} width={400} /></h1>
+          <h1><img alt='a logo for buttheads' src={image} height={100} width={400} /></h1>
         </Link>
 
         <nav className="text-center">
