5167ddbaa41732c3b95105947686d50550296b7b
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-09-21 23:17:54 -0500

    style

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,13 +11,13 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="flex-row justify-space-between-lg justify-center align-center">
+    <header className="bg-secondary mb-5 py-2 flex-row align-center">
+      <div className="flex-row justify-space-between-lg justify-center align-center ">
         <Link to="/home">
           <h1><img alt='a logo for buttheads' src={image} height={100} width={400} /></h1>
         </Link>
 
-        <nav className="text-center">
+        <nav className="text-center mb-5">
           {Auth.loggedIn() ? (
             <>
               <Link to="/mycard">My Card</Link>
