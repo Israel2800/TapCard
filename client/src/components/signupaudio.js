d141524e31d4f873cbb6e186d0950c6733f2f88d
Israel Aguilar <dady281100@gmail.com>
2023-02-27 23:56:46 -0600

    Will work on signupaudio.js file in the future when styles are done

--- client/src/components/signupaudio.js
+++ client/src/components/signupaudio.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import mouseoversub from "../audio/mouseoversub.mp3";
+// import React, { Component } from "react";
+// import mouseoversub from "../audio/mouseoversub.mp3";
 
-const audio = [
-    {sound:mousevoversub},
-]
+// const audio = [
+//     {sound:mousevoversub},
+// ]
 
-Class App extends Component {
-    renderMatches() {
-        return <div className="sound1"></div>
-    }
-}
+// Class App extends Component {
+//     renderMatches() {
+//         return <div className="sound1"></div>
+//     }
+// }
 
 
-export default signupaudio;
+// export default signupaudio;
